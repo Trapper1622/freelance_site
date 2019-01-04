9389e00b3d0f8c0c3b1a838ef267a6725fb1dff7
Trapper1622 <titoutitiwow@gmail.com>
2019-01-04 14:59:01 +0100

    About us

--- aboutUs.php
+++ aboutUs.php
@@ -13,7 +13,7 @@
 
                         <div class="swiper-wrapper">
 
-                            <div class="swiper-slide" style="background-image: url(img/background-106.jpg);">
+                            <div class="swiper-slide" style="background-image: url(img/bg_about_us.jpg);">
                                 <div class="full-size valign-middle">
                                     <div class="valign-text-wrapper text-center">
                                         <div class="slide-title h2"><b>A propos</b></div>
@@ -24,7 +24,7 @@
                                     </div>
                                 </div>
                             </div>
-
+                        
                         </div>
                         <div class="swiper-pagination visible-xs"></div>
                     </div>
@@ -45,18 +45,48 @@
                     <div class="h4"><b>LOREM IPSUM DOLOR SEMPER AMET, CONSECTETUR ELIT. INTEGER SEMPER ELIT NEC JUSTO AUCTOR</b></div>
                     <div class="col-xs-b15"></div>
                     <div class="simple-article grey large">
-                        <p>Phasellus mattis magna vel augue gravida eleifend. Donec sit amet diam mollis, sodales lorem, blandit orci. Proin nec mauris lectus. Mauris sit amet ex et nibh dapibus gravida. Sed luctus ipsum purus vitae dictum </p>
+                        <p>Développeur front-end Freelance sur Montpellier, je suis passionné par les nouvelles technologies et encore plus par la conception web. 
+
+                        Je suis en perpétuel apprentissage afin d’améliorer au mieux mes compétences. Je suis spécialisé dans l’intégration et le développement Front-end mais aussi dans le développement de site wordpress. 
+
+                        Je travaille de façon structurée pour que mon code ou les projets puissent être compris et réutilisés.
+                        J’accorde une grande importance au besoin et à la satisfaction du client. 
+
+                        J’ai commencé en autodidacte pour ensuite faire une formation de développeur web afin de solidifier mes compétences dans le développement front-end et back-end.
+
+                        Je vous invite à découvrir mes différentes réalisations. ( mon portfolio : tristanb.fr )
+
+                        Je suis Motivé par le besoin de faire toujours mieux et appuyé par de solides bases acquises lors de mon parcours ( Javascript, php, html, CSS, Wordpress ), je prends plaisir à relever de nouveaux challenges.
+
+                        <p><center>Voici mes domaines de compétences :</center></p>
+                        </p>
                     </div>
                     <div class="empty-space col-xs-b35"></div>
                     <div class="row">
                         <div class="col-sm-6 text-left">
                             <div class="simple-article grey">
-                                <p>Aliquam vulputate turpis quis nibh tempus finibus. In fringilla nulla quis lacus sagittis, sed malesuada enim faucibus. Nulla interdum dolor mauris, vitae molestie dui bibendum et. Praesent non felis in sem placerat mattis eget et justo. Fusce quis justo et arcu tristique gravida sit amet</p>
+                                <p>Design :
+                                    <br>
+                                - Adobe Photoshop
+                                <br>
+                                - Adobe XD
+                                </p>
                             </div>
                         </div>
                         <div class="col-sm-6 text-left">
                             <div class="simple-article grey">
-                                <p>Donec purus lacus, vehicula vitae posuere quis, dignissim ut lectus. Fusce et purus malesuada, vulputate nisi id, convallis purus. Nam porta ipsum at consequat molestie. In nunc nisl, gravida sed tortor id, consequat mollis quam. Quisque accumsan aucto nisi id, convallis purus</p>
+                                <p>Développement web :
+                                    <br>
+                                - Intégration HTML5 / CSS3 ( responsive )<br>
+                                - Bootstrap 4<br>
+                                - SASS<br>
+                                - Javascript<br>
+                                - Reactjs<br>
+                                - Git<br>
+                                - Wordpress ( Création de site web, Installation et Configuration de thème, Création de thème, Installation et Configuration de plugins, création de plugin etc )<br>
+                                - PHP<br>
+                                - MySQL
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -64,55 +94,8 @@
                     <a href="contact.php" class="button type-3">CONTactez moi</a>
                 </div>
             </div>
-
-            <div class="empty-space col-xs-b55 col-sm-b110"></div>
-
-            <div class="swiper-container">
-                <div class="swiper-button-prev swiper-button hidden"></div>
-                <div class="swiper-button-next swiper-button hidden"></div>
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="text-center">
-                            <img class="blockquote-image" src="img/detail-30.jpg" alt="">
-                            <div class="empty-space col-xs-b25"></div>
-                            <div class="h5 small"><b>“A GOOD MODEL CAN ADVANCE FASHION BY TEN YEARS”</b></div>
-                            <div class="empty-space col-xs-b15"></div>
-                            <div class="simple-article grey"><span class="inline-indent"></span>Yves Saint Laurent, fashion gesigner</div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="text-center">
-                            <img class="blockquote-image" src="img/detail-30.jpg" alt="">
-                            <div class="empty-space col-xs-b25"></div>
-                            <div class="h5 small"><b>“A GOOD MODEL CAN ADVANCE FASHION BY TEN YEARS”</b></div>
-                            <div class="empty-space col-xs-b15"></div>
-                            <div class="simple-article grey"><span class="inline-indent"></span>Yves Saint Laurent, fashion gesigner</div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="text-center">
-                            <img class="blockquote-image" src="img/detail-30.jpg" alt="">
-                            <div class="empty-space col-xs-b25"></div>
-                            <div class="h5 small"><b>“A GOOD MODEL CAN ADVANCE FASHION BY TEN YEARS”</b></div>
-                            <div class="empty-space col-xs-b15"></div>
-                            <div class="simple-article grey"><span class="inline-indent"></span>Yves Saint Laurent, fashion gesigner</div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="text-center">
-                            <img class="blockquote-image" src="img/detail-30.jpg" alt="">
-                            <div class="empty-space col-xs-b25"></div>
-                            <div class="h5 small"><b>“A GOOD MODEL CAN ADVANCE FASHION BY TEN YEARS”</b></div>
-                            <div class="empty-space col-xs-b15"></div>
-                            <div class="simple-article grey"><span class="inline-indent"></span>Yves Saint Laurent, fashion gesigner</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-pagination relative-pagination" style="margin-top: 20px;"></div>
-            </div>
         </div>
-
-        <div class="empty-space col-xs-b55 col-sm-b110"></div>
+            <div class="empty-space col-xs-b55 col-sm-b110"></div>
 
         <div class="container">
             <div class="row">
